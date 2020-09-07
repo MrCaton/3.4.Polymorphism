71e78bbcc830197cb60526d64be41d314089fc2a
Iván Araque Cristóbal <ivanaraquecristobal@gmail.com>
2020-09-07 09:47:43 +0200

    Add project files.

---
+++ 3.4.Polymorphism/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._4.Polymorphism
+{
+    class Program
+    {
+        //UserError userError=new UserError();
+        static void Main(string[] args)
+        {
+                Console.ReadLine();
+                UserError text = new NumericInputError();
+                UserError number = new TextInputError();
+                text.UEMessage();
+                number.UEMessage();
+            
+            
+
+
+
+            //foreach (var userError in UserError)
+            //{
+
+            //}
+        }
+    }
+    abstract class UserError
+    {
+        // private List<UserError> userError;
+        public virtual  string UEMessage()
+        {
+            return "Error";
+        }
+        //public UserError()
+        //{
+        //    userError = new List<UserError>();
+        //}
+    }
+    class NumericInputError:UserError
+    {
+ 
+        public override string UEMessage()
+        {
+            return "You tried to use a text input in a numeric only field. This fired an error!";
+
+        }
+    }
+    class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a numeric input in a text only field. This fired an error!";
+
+        }
+    }
+}
